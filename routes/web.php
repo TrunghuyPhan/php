0de948fb4ae8e6cac544fc045b346372012a46f4
Trunghuyphan <hoanghac1999@gmail.com>
2021-03-11 14:52:23 +0700

    Create1

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::get('/', function () {
-//     return view('layuot');
+//     return view('welcome');
 // });
 Route::get('/','Controller@Index')->name("home");
