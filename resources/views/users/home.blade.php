1edb9e5d8588198f32c37ecd7ac31353b6ce70d0
Trunghuyphan <hoang.hac1999@gmail.com>
2021-03-15 04:24:44 +0700

    load data on home

---
+++ resources/views/users/home.blade.php
@@ -0,0 +1,53 @@
+@extends('users.layout')
+@section('content')
+<div class="banner header-text">
+  <div class="owl-banner owl-carousel">
+    <div class="banner-item-01">
+      <div class="text-content">
+        <h4>Best Offer</h4>
+        <h2>New Arrivals On Sale</h2>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="latest-products">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="section-heading">
+          <h2>Sản phẩm mới nhất</h2>
+          <a href="products.html">Xem tất cả <i class="fa fa-angle-right"></i></a>
+        </div>
+      </div>
+      @foreach($all_product as $key => $product)
+      <div class="col-md-4">
+        <div class="product-item">
+          <a href="#"><img src="{{URL::to('/img/'.$product->product_image)}}" alt=""></a>
+          <div class="down-content">
+            <div class="title">
+              <a href="#">
+                <h4>{{$product->product_name}}</h4>
+              </a>
+            </div>
+           <div class="price">
+             <h6>{{$product->product_price/1000}}.000VNĐ</h6>
+           </div>
+            
+            <ul class="stars">
+              <li><i class="fa fa-star"></i></li>
+              <li><i class="fa fa-star"></i></li>
+              <li><i class="fa fa-star"></i></li>
+              <li><i class="fa fa-star"></i></li>
+              <li><i class="fa fa-star"></i></li>
+            </ul>
+            <span>Reviews (24)</span>
+          </div>
+        </div>
+      </div>
+      @endforeach
+    </div>
+  </div>
+</div>
+
+@endsection
