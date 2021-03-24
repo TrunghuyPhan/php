3d6f06f71526fcaa57cbcf04d12b7eb855a05893
phucs2612 <phuclx123456@gmail.com>
2021-03-24 20:46:10 +0700

    commit

---
+++ app/CategoryProduct.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryProduct extends Model
+{
+    //
+    protected $table = "tbl_category_product";
+    protected $primaryKey = "category_id";
+}
